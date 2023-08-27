22e3e44a558de9daa74d15b2515fea2c4e528ceb
Hasan-Rifat <developerhr000@gmail.com>
2023-08-27 16:41:52 +0600

    [header updateded]

---
+++ src/pages/pc-builder/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type PcBuilderProps = {};
+
+const PcBuilder: React.FC<PcBuilderProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default PcBuilder;
---
+++ src/pages/product-details/[id].tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type ProductDetailsProps = {};
+
+const ProductDetails: React.FC<ProductDetailsProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default ProductDetails;
